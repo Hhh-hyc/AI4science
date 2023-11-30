29f8e7ba82e0f73d1033472ad7b0a204f6368335
Yisreal <huyucong34@gmail.com>
2023-11-30 09:26:44 +0800

    deeponet

--- docs/sw.js
+++ docs/sw.js
@@ -1,6 +1,6 @@
 self.importScripts('/AI4science/assets/js/data/swcache.js');
 
-const cacheName = 'chirpy-1699795426';
+const cacheName = 'chirpy-1701269441';
 
 function verifyDomain(url) {
   for (const domain of allowedDomains) {
